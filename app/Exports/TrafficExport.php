05cc80a05c0fe8201e028afabd0aac7f0e28dfff
nauvalalpen <nauvalalpenperdana@gmail.com>
2026-04-26 18:23:51 +0700

    Refactor TrafficExport to implement FromCollection and add mapping and headings for Excel export

--- app/Exports/TrafficExport.php
+++ app/Exports/TrafficExport.php
@@ -2,24 +2,60 @@
 
 namespace App\Exports;
 
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class TrafficExport implements FromView, ShouldAutoSize
+class TrafficExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize, WithStyles
 {
     protected $traffics;
+    private $rowNumber = 0; // Untuk bikin nomor urut otomatis
 
     public function __construct($traffics)
     {
         $this->traffics = $traffics;
     }
 
-    public function view(): View
+    // 1. Mengambil datanya
+    public function collection()
     {
-        // Akan memanggil file blade cetak.blade.php
-        return view('laporan.cetak',[
-            'traffics' => $this->traffics
-        ]);
+        return $this->traffics;
+    }
+
+    // 2. Mapping (Memasukkan data ke dalam baris Excel)
+    public function map($traffic): array
+    {
+        return[
+            ++$this->rowNumber,
+            $traffic->tanggal,
+            $traffic->kamera->area->nama_area ?? '-',
+            $traffic->kamera->nama_kamera ?? '-',
+            $traffic->kendaraan_masuk,
+            $traffic->kendaraan_keluar,
+        ];
+    }
+
+    // 3. Membuat Judul Kolom (Header)
+    public function headings(): array
+    {
+        return[
+            'No',
+            'Tanggal',
+            'Lokasi Area',
+            'Kamera CCTV',
+            'Kendaraan Masuk',
+            'Kendaraan Keluar',
+        ];
+    }
+
+    // 4. (Opsional) Menebalkan huruf di baris pertama (Header)
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+        ];
     }
 }
