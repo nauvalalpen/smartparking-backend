174e484c0f3032d7b525d8a73852439584b65eda
nauvalalpen <nauvalalpenperdana@gmail.com>
2026-05-21 11:11:36 +0700

    fix : area controller

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -55,7 +55,7 @@ class AreaController extends Controller
         ]);
 
         Area::create([
-            'id_pengguna' => auth()->id(),
+            'id_pengguna' => $request->user()->id,
             'nama_area' => $request->nama_area,
             'deskripsi' => $request->deskripsi,
             'kapasitas_total' => $request->kapasitas_total
